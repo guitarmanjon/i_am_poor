7ee48c09578bc1c4d26ed9fe45f02d3f049f0e99
guitarmanjon <guitarmanjon@yahoo.com>
2019-11-25 13:44:47 +0000

    First build of app, main image in app needs fixing

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.red[400],
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange[200],
+        title: Center(child: Text('I am poor')),
+      ),
+      body: Center(
+        child: Image(
+          // TODO: Work out why the below image is not showing in the app.
+          image: AssetImage('images/I_am_poor_app_icon.png'),
+        ),
+      ),
+    ),
+  ));
+}
